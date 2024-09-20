35b414fc270a17599972b3f7ba2178cc858f73e8
서휘원 <shw445567@naver.com>
2024-09-20 22:41:39 +0900

    url 변경시 리렌더링 추가
    URL이 변경
    > useParams가 다시 호출되어 postId 값이 변경
    > useEffect 동작
    > 리렌더링(가상 DOM Diffing)

--- src/post.jsx
+++ src/post.jsx
@@ -11,8 +11,7 @@ function Post() {
             .then((response) => response.json())
             .then((data) => setData(data))
             .catch((error) => console.log(error));
-    }, []);
-
+    }, [postId]); // URL이 변경되면 useParams가 다시 호출되어 postId 값이 변경 > useEffect 동작 > 리렌더링(가상 DOM Diffing)
     return (
         <div id={styles.container}>
             <div id={styles.main}>
