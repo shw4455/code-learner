168dc09ab92f178f4da97acb39d2dc8030ad7f4d
서휘원 <shw445567@naver.com>
2024-10-02 00:42:33 +0900

    createPost 컴포넌트 UI 수정 및 Button 컴포넌트 추가

--- src/createPost.jsx
+++ src/createPost.jsx
@@ -1,24 +1,32 @@
-import axios from "axios";
-import React from "react";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import styles from "./styles/createPost.module.css";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import DeletePost from "./commponent/DeletePost.jsx";
+import axios from "axios";
+import Button from "./commponent/Button.jsx";
 
-const CreatePost = () => {
-    // const [post, setPost] = useState({
-    //     title: "",
-    //     content: "",
-    // });
+function CreatePost() {
+    const { postId } = useParams();
 
     const [post, setPost] = useState({
-        title: "222",
-        content: "222",
+        title: "",
+        content: "",
     });
-
     const [error, setError] = useState(false); // ?
-
     const navigate = useNavigate(); // ?
+    useEffect(() => {
+        fetch(`http://localhost:3001/api/posts/${postId}`)
+            .then((response) => response.json())
+            .catch((error) => console.log(error));
+    }, [postId]); // URL이 변경되면 useParams가 다시 호출되어 postId 값이 변경 > useEffect 동작 > 리렌더링(가상 DOM Diffing)
 
+    const [showDeleted, setShowDeleted] = useState(false);
+    const handleDelete = () => {
+        setShowDeleted(true); // 삭제 버튼 클릭 시 showDeleted 상태를 true로 변경
+    };
+    const handleCancel = () => {
+        setShowDeleted(false);
+    };
     const handleChange = (e) => {
         setPost((prev) => ({ ...prev, [e.target.name]: e.target.value }));
         console.log("Create input change");
@@ -27,35 +35,47 @@ const CreatePost = () => {
 
     const handleClick = async (e) => {
         try {
-            await axios.post("http://localhost:3001/api/post", post);
+            await axios
+                .post("http://localhost:3001/api/post", post)
+                .then(navigate("/"));
         } catch (err) {
             console.log(err);
         }
     };
 
     return (
-        <div className={styles.createContainer}>
-            <div className={styles.form}>
-                <h1>CreatePost New Book</h1>
-                <input
-                    type="text"
-                    placeholder="Book title"
-                    name="title"
-                    onChange={handleChange}
-                />
-                <textarea
-                    rows={5}
-                    type="text"
-                    placeholder="Book desc"
-                    name="content"
-                    onChange={handleChange}
-                />
-                <button onClick={handleClick}>CreatePost</button>
-                {error && "Something went wrong!"}
-                <Link to="/board">See all books</Link>
+        <div id={styles.container}>
+            <div id={styles.main}>
+                <div id={styles.titleContainer}>
+                    <input
+                        id={styles.titleInput}
+                        type="text"
+                        placeholder="제목을 입력하세요"
+                        name="title"
+                        onChange={handleChange}
+                    />
+                </div>
+                <hr className={styles.divider}></hr>
+                <div id={styles.postContentContainer}>
+                    <textarea
+                        rows={10}
+                        type="text"
+                        placeholder="본문을 입력하세요."
+                        name="content"
+                        onChange={handleChange}
+                    />
+                </div>
+                <hr></hr>
+                <div className={styles.buttonWrapper}>
+                    <Button
+                        onClick={handleClick}
+                        buttonText={"작성하기"}
+                    ></Button>
+                    {error && "Something went wrong!"}
+                </div>
             </div>
         </div>
     );
-};
+}
 
 export default CreatePost;
